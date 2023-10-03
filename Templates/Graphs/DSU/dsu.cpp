0462adce034666af7381dc2ffea03c21747c0073
tanishq2507 <smartlohia@gmail.com>
2023-10-04 05:22:29 +0530

    Create dsu.cpp

---
+++ Templates/Graphs/DSU/dsu.cpp
@@ -0,0 +1,23 @@
+int find_parent(int node,vector<int>& parent){
+    if(parent[node]==node)
+        return(node);
+    return(parent[node]=find_parent(parent[node],parent));
+}
+
+
+void make_union(int u,int v,vector<int>& sze,vector<int>&parent){
+    int up1,up2;
+    up1 = find_parent(u,parent);
+    up2 = find_parent(v,parent);
+    if(up1!=up2){
+        if(sze[up1]>=sze[up2]){
+            parent[up2]=up1;
+            sze[up1]+=sze[up2];
+        }
+        else{
+            parent[up1]=up2;
+            sze[up2]+=sze[up1];
+        }
+    }
+    
+}
