519c1cf386a8caa91732e843058039348d35ab69
Tanishq Lohia <93226718+tanishq2507@users.noreply.github.com>
2023-12-25 12:51:43 +0530

    Update kushkal's_algo.cpp

--- Templates/Graphs/Minimum_spanning_tree/kushkal's_algo.cpp
+++ Templates/Graphs/Minimum_spanning_tree/kushkal's_algo.cpp
@@ -1,3 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=  long long;
+#define MOD 1000000007
+#define endl "\n"
+
 int find_parent(int node,vector<int>& parent){
     if(parent[node]==node)
         return(node);
@@ -28,16 +34,17 @@ int make_union(vector<int>edgex,vector<int>& sze,vector<int>&parent){
     
 }
 
-ll mst_weight(vector<vector<int>>&edges2,vector<int>& parent,vector<int>& sze){
+ll mst_weight(vector<vector<int>>&edges,vector<int>& parent,vector<int>& sze){
 //Krushkal's algo
+//edges must be sorted in increasing order.
         int i;
         ll c=0;
-        for(i=0;i<edges2.size();i++){
-            c+=(ll)make_union(edges2[i],sze,parent);
+        for(i=0;i<edges.size();i++){
+            c+=(ll)make_union(edges[i],sze,parent);
         }
         return(c);
     }
-
+    
 bool is_mst(int n,vector<int>& parent){
         int i ;
         int par = find_parent(0,parent);
@@ -47,3 +54,43 @@ bool is_mst(int n,vector<int>& parent){
         }
         return(true);
     }
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    int t=1;
+    //cin>>t;
+    int xxx=1;
+    
+    while(t--){
+        int n,m,i;
+        cin>>n>>m;
+        
+        vector<vector<int>>edges;
+        
+        for(i=0;i<m;i++){
+            int u,v,w;
+            cin>>u>>v>>w;
+            u--;
+            v--;
+            edges.push_back({w,u,v});
+        }
+        vector<int>parent(n);
+        vector<int>sze(n,1);
+        sort(edges.begin(),edges.end());
+        for(i=0;i<n;i++)
+            parent[i] = i;
+        
+        ll ans = mst_weight(edges,parent,sze);
+        
+        if(is_mst(n,parent))
+            cout<<ans<<endl;
+        else
+            cout<<"IMPOSSIBLE"<<endl;
+        
+    }
+
+   return 0;
+}
