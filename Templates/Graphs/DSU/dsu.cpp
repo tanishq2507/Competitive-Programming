06ea0e133fe23fead25a64c8d35888ca28b0a1e0
Tanishq Lohia <93226718+tanishq2507@users.noreply.github.com>
2024-06-30 20:58:04 +0530

    Update dsu.cpp

--- Templates/Graphs/DSU/dsu.cpp
+++ Templates/Graphs/DSU/dsu.cpp
@@ -1,23 +1,41 @@
-int find_parent(int node,vector<int>& parent){
-    if(parent[node]==node)
-        return(node);
-    return(parent[node]=find_parent(parent[node],parent));
-}
-
-//always find parent using find_parent function
-void make_union(int u,int v,vector<int>& sze,vector<int>&parent){
-    int up1,up2;
-    up1 = find_parent(u,parent);
-    up2 = find_parent(v,parent);
-    if(up1!=up2){
-        if(sze[up1]>=sze[up2]){
-            parent[up2]=up1;
-            sze[up1]+=sze[up2];
-        }
-        else{
-            parent[up1]=up2;
-            sze[up2]+=sze[up1];
+class DSU {
+public:
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        numComponents = n;
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
         }
     }
-    
-}
+
+    int find_parent(int node) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = find_parent(parent[node]);
+    }
+
+    void make_union(int u, int v) {
+        int up1 = find_parent(u);
+        int up2 = find_parent(v);
+        if (up1 != up2) {
+            if (size[up1] >= size[up2]) {
+                parent[up2] = up1;
+                size[up1] += size[up2];
+            } else {
+                parent[up1] = up2;
+                size[up2] += size[up1];
+            }
+            numComponents--; // Decrement number of components when union is performed
+        }
+    }
+
+    int getNumComponents() const {
+        return numComponents;
+    }
+
+private:
+    std::vector<int> parent;
+    std::vector<int> size;
+    int numComponents;
+};
